c7574cc08624099f1683c48eaed8f41913debf60
Michael David <lordkay07@gmail.com>
2025-07-23 21:58:23 +0100

    setup db connection with sequelize securely

---
+++ backend/dev/config/database.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize({
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    logging: console.log,
+})
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection to the database has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
